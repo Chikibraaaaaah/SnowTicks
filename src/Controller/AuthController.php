9d70f1700f8e21719e0a1ddf5d94000e6500b16d
Tristan Riedinger <tristanriedinger@gmail.com>
2024-03-27 15:57:29 +0100

    Vue harmonisées
    La perte du head était liée à la nommenclature du block twig dans laquelle se trouvait l'élément.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -23,54 +23,25 @@ class AuthController extends AbstractController
     }
 
 
-    #[Route('/auth', name: 'app_auth', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/auth/register', name: 'app_auth_register', methods: ['GET'])]
+    public function register(): Response
     {
-
         return $this->render('auth/auth.twig', [
-            'controller_name' => 'AuthController',
+            "title" => "Connectez-vous en 3 clics",
+            "method" => "login",
+            "user" => []
         ]);
     }
-
-    // #[Route('/auth', name: 'app_auth_register', methods: ['GET'])]
-    // public function index(): Response
-    // {
-
-    //     return $this->render('auth/auth.twig', [
-    //         'controller_name' => 'AuthController',
-    //     ]);
-    // }
-
 
     #[Route('/auth/create', name: 'app_auth_create', methods: ['POST'])]
-    public function create()
+    public function create(Request $request): Response
     {
 
+        dd($request);
 
-        $email = $_POST['email'];
-
-        var_dump($email);
-
-
-        $user = $this->repository->findBy(["email" => $email]);
-        var_dump($user);
 
         die();
-
-
     }
 
 
-    #[Route('/auth/signup', name: 'app_auth_signup', methods: ['GET'])]
-    public function signup()
-    {
-
-        
-        return $this->render('auth/signup.twig', [
-            "user" => [],
-            "title" => "Inscription" 
-        ]);
-
-
-    }
 }
